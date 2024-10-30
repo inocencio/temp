de00e3180de47c12c413636dd93efbebbf28e251
Inocencio <inocencioto@gmail.com>
2024-10-30 00:40:22 -0300

    Update qw.bat

--- qw/qw.bat
+++ qw/qw.bat
@@ -1,5 +1,6 @@
 @echo off
 
 set "args=%*"
+set "currentDir=%~dp0"
 
-"C:\Program Files\Java\jdk-17.0.1\bin\java.exe" -Dfile.encoding=UTF-8 -jar C:\dev\projects\outros\qw\target\qw.jar %args%
+"java.exe" -Dfile.encoding=UTF-8 -jar "%currentDir%\target\qw.jar" %args%
